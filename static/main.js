707089243bb0446ff3d3ae52047372811e886ab3
Roman Makarevich <remmybump@gmail.com>
2023-03-25 16:01:18 +0300

    fixed socket io path

--- static/main.js
+++ static/main.js
@@ -26,7 +26,7 @@ const app = new Vue({
         }
     },
     created() {
-        this.socket = io('http://localhost:3000')
+        this.socket = io(location.href)
         this.socket.on('msgToClient', (message) => {
             this.receivedMessage(message)
         })
